7173fd4a01355ec6eded8813fe362946cf2385d5
Linus Torvalds <torvalds@linux-foundation.org>
2026-03-04 07:37:13 +0800

    改善一下返回错误时的语言

--- back/src/handler/logic/errors.go
+++ back/src/handler/logic/errors.go
@@ -6,15 +6,15 @@ import "errors"
 // 多个逻辑可能共享这些错误定义
 var (
 	//注册时，数据库中不存在该学号对应的记录(data.students)
-	ErrNoStudentRecord = errors.New("抱歉，您输入的姓名或学号有误，如果确信所输入信息没有问题，请联系我们的工作人员。")
+	ErrNoStudentRecord = errors.New("您输入的姓名或学号有误，如果确信所输入信息没有问题，请联系我们的工作人员。")
 	//注册时，所提供的学号-姓名不匹配数据库记录
-	ErrSidNameNotMatch = errors.New("抱歉，您输入的姓名或学号有误，如果确信所输入信息没有问题，请联系我们的工作人员。")
+	ErrSidNameNotMatch = errors.New("您输入的姓名或学号有误，如果确信所输入信息没有问题，请联系我们的工作人员。")
 	//注册时，提供的学号-姓名所对应的用户已经注册了
 	ErrUserAlreadyRegistered = errors.New("您已经注册了。如果您确信您还没有注册，请联系我们的工作人员。")
 	//该联系电话号码已在数据库中被使用注册
-	ErrPhoneUsed = errors.New("抱歉，您所使用的联系电话已经被登记，请换一个不一样的电话号码。")
+	ErrPhoneUsed = errors.New("您所使用的联系电话已经被登记，请换一个不一样的电话号码。")
 	//注册时，该微信号已被使用注册
-	ErrWxUsed = errors.New("抱歉，您的微信已经注册过了，一个微信只能注册一个账号。")
+	ErrWxUsed = errors.New("您的微信已经注册过了，一个微信只能注册一个账号。")
 	// 真的。。。会出现这种错误吗？
 	ErrDataInconsistent = errors.New("创建用户时数据库返回数据与请求数据不一致，请联系我们的技术团队。")
 	// 根据OpenID查不到用户
@@ -24,7 +24,7 @@ var (
 	// 故障发生时间晚于现在了
 	ErrOccurAtTimeInvalid = errors.New("请填写有效的故障发生时间")
 	// 不允许用户创建太多没有关闭的工单，设置成~3~个，有需要可以改
-	ErrTicketTooMuch = errors.New("抱歉，您当前还有正在活跃的报修，无法创建新报修")
+	ErrTicketTooMuch = errors.New("您当前还有正在活跃的报修，无法创建新报修")
 	// 根据工单ID查不到工单
 	ErrNoSuchTicket = errors.New("无法找到对应的工单")
 	// 根据网维成员ID查不到网维成员
