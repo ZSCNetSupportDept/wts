e69e7219a27dd19e98ef5af6d871a97561b5d792
Linus Torvalds <torvalds@linux-foundation.org>
2026-04-19 09:00:29 +0800

    修改了一下用户菜单栏，增加联系方式的页面

---
+++ front/src/routes/about/contact/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+        import { SUPPORT_QQ } from "$lib/env/businesses";
+</script>
+
+<h1>联系我们</h1>
+<br />
+<hr />
+<br />
+<p style="font-size: 0.95em;">
+        对报修有疑问？对网维有建议？或者只是想和我们聊聊天？只要与校园网和网络维护科本身相关的问题，我们都期待与您的交流。欢迎通过以下方式联系我们：
+</p>
+
+<br/>
+<p>
+        <span><strong>QQ群：</strong>{SUPPORT_QQ}</span>
+        <br/>
+        <span style="color: gray; font-size: 0.9em;">有问题直接在群里说就行，也可以私聊任意群管理</span>
+</p>
+
+<br/>
+<p>
+        <span><strong>微信号：</strong>zsxywwbx（中山学院网维报修系统）</span>
+        <br/>
+        <span style="color: gray; font-size: 0.9em;">可以通过微信搜索公众号“中山学院网维报修系统”添加关注</span>
+</p>
